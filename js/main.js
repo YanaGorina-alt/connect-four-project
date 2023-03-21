45d901a0e96c7f7a929318803ca1a7186e47464a
Yana Gorina <yagorina.ne@gmail.com>
2023-03-20 20:12:32 -0700

    Made pseudo code and game board

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 // Pseudo Code:
 // Create a game board 6 rows / 7 columns (grid of 42 cells) in HTML or in JS (for loop)
-// Make two players
+// Make two players (make one player and then switch)
 // Listen for a player to click on a board
 // Check if it is an appropriate cell in a board to place a disk (must be free, and be on top of another disk)
 // Check if a player won (four disks in horizontal/diagonal/vertical line). Use all possible  win patterns to compare with=>
@@ -10,6 +10,9 @@
 // Offer to play again
 
 let gameBoard = document.querySelector('.gameBoard');
+let player = document.querySelector('.player-id');
+
+
 
 function makeGameBoard(){
     for(let i= 0; i < 42; i++){
